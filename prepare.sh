f4f2b2baf2e5c7a13bae3a8e74f8fa5248c9e583
sagia <sagia@rapyd.net>
2022-03-30 14:19:33 +0300

    Edited build scripts

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 cd ../dynamic-dashboard-front
+npm install
 npm run build
 cd ../dynamic-dashboard-back
 docker start dashboard-mongodb || docker run -d -p 27017:27017 -v /home/vbboard1/mongodb/:/data/db --name dashboard-mongodb gutsagi/dashboard:base
